ae2652f5b12dae2e6ae389f48f7b2e386f63497e
KILLROY\Nicholaus <prin2096@hotmail.com>
2018-07-14 11:31:18 -0700

    New Master Project and TK Mechanic created

---
+++ MasterProjectCPP/LianeGame/Intermediate/Build/Win64/UE4Editor/Inc/LianeGame/LianeGameCharacter.gen.cpp
@@ -0,0 +1,186 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "LianeGameCharacter.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeLianeGameCharacter() {}
+// Cross Module References
+	LIANEGAME_API UClass* Z_Construct_UClass_ALianeGameCharacter_NoRegister();
+	LIANEGAME_API UClass* Z_Construct_UClass_ALianeGameCharacter();
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+	UPackage* Z_Construct_UPackage__Script_LianeGame();
+	LIANEGAME_API UFunction* Z_Construct_UFunction_ALianeGameCharacter_MoveActor();
+	ENGINE_API UClass* Z_Construct_UClass_UPhysicsHandleComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+// End Cross Module References
+	void ALianeGameCharacter::StaticRegisterNativesALianeGameCharacter()
+	{
+		UClass* Class = ALianeGameCharacter::StaticClass();
+		static const FNameNativePtrPair Funcs[] = {
+			{ "MoveActor", &ALianeGameCharacter::execMoveActor },
+		};
+		FNativeFunctionRegistrar::RegisterFunctions(Class, Funcs, ARRAY_COUNT(Funcs));
+	}
+	UFunction* Z_Construct_UFunction_ALianeGameCharacter_MoveActor()
+	{
+		static UFunction* ReturnFunction = nullptr;
+		if (!ReturnFunction)
+		{
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Function_MetaDataParams[] = {
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Called to move a grabbed object back/forward" },
+			};
+#endif
+			static const UE4CodeGen_Private::FFunctionParams FuncParams = { (UObject*(*)())Z_Construct_UClass_ALianeGameCharacter, "MoveActor", RF_Public|RF_Transient|RF_MarkAsNative, nullptr, (EFunctionFlags)0x00040401, 0, nullptr, 0, 0, 0, METADATA_PARAMS(Function_MetaDataParams, ARRAY_COUNT(Function_MetaDataParams)) };
+			UE4CodeGen_Private::ConstructUFunction(ReturnFunction, FuncParams);
+		}
+		return ReturnFunction;
+	}
+	UClass* Z_Construct_UClass_ALianeGameCharacter_NoRegister()
+	{
+		return ALianeGameCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_ALianeGameCharacter()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_ACharacter,
+				(UObject* (*)())Z_Construct_UPackage__Script_LianeGame,
+			};
+			static const FClassFunctionLinkInfo FuncInfo[] = {
+				{ &Z_Construct_UFunction_ALianeGameCharacter_MoveActor, "MoveActor" }, // 3616483142
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Navigation" },
+				{ "IncludePath", "LianeGameCharacter.h" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+			};
+#endif
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_PhysicsHandle_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Telekinesis" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_PhysicsHandle = { UE4CodeGen_Private::EPropertyClass::Object, "PhysicsHandle", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, PhysicsHandle), Z_Construct_UClass_UPhysicsHandleComponent_NoRegister, METADATA_PARAMS(NewProp_PhysicsHandle_MetaData, ARRAY_COUNT(NewProp_PhysicsHandle_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_ThrowStrength_MetaData[] = {
+				{ "Category", "Telekinesis" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "The strength of the telekinesis throw" },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_ThrowStrength = { UE4CodeGen_Private::EPropertyClass::Float, "ThrowStrength", RF_Public|RF_Transient|RF_MarkAsNative, 0x0040000000000001, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, ThrowStrength), METADATA_PARAMS(NewProp_ThrowStrength_MetaData, ARRAY_COUNT(NewProp_ThrowStrength_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_PullDirection_MetaData[] = {
+				{ "Category", "Telekinesis" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "The direction which the grabbed object is pulled" },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_PullDirection = { UE4CodeGen_Private::EPropertyClass::Float, "PullDirection", RF_Public|RF_Transient|RF_MarkAsNative, 0x0040000000000001, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, PullDirection), METADATA_PARAMS(NewProp_PullDirection_MetaData, ARRAY_COUNT(NewProp_PullDirection_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_TK_Reach_MetaData[] = {
+				{ "Category", "Telekinesis" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "How far ahead of the player can we reach in cm" },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_TK_Reach = { UE4CodeGen_Private::EPropertyClass::Float, "TK_Reach", RF_Public|RF_Transient|RF_MarkAsNative, 0x0040000000000001, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, TK_Reach), METADATA_PARAMS(NewProp_TK_Reach_MetaData, ARRAY_COUNT(NewProp_TK_Reach_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BaseLookUpRate_MetaData[] = {
+				{ "Category", "Camera" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Base look up/down rate, in deg/sec. Other scaling may affect final rate." },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_BaseLookUpRate = { UE4CodeGen_Private::EPropertyClass::Float, "BaseLookUpRate", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000020015, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, BaseLookUpRate), METADATA_PARAMS(NewProp_BaseLookUpRate_MetaData, ARRAY_COUNT(NewProp_BaseLookUpRate_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BaseTurnRate_MetaData[] = {
+				{ "Category", "Camera" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Base turn rate, in deg/sec. Other scaling may affect final turn rate." },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_BaseTurnRate = { UE4CodeGen_Private::EPropertyClass::Float, "BaseTurnRate", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000020015, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, BaseTurnRate), METADATA_PARAMS(NewProp_BaseTurnRate_MetaData, ARRAY_COUNT(NewProp_BaseTurnRate_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_TKCamera_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Telekinesis camera" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_TKCamera = { UE4CodeGen_Private::EPropertyClass::Object, "TKCamera", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, TKCamera), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(NewProp_TKCamera_MetaData, ARRAY_COUNT(NewProp_TKCamera_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_FollowCamera_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Follow camera" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_FollowCamera = { UE4CodeGen_Private::EPropertyClass::Object, "FollowCamera", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, FollowCamera), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(NewProp_FollowCamera_MetaData, ARRAY_COUNT(NewProp_FollowCamera_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_CameraBoom_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "LianeGameCharacter.h" },
+				{ "ToolTip", "Camera boom positioning the camera behind the character" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_CameraBoom = { UE4CodeGen_Private::EPropertyClass::Object, "CameraBoom", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(ALianeGameCharacter, CameraBoom), Z_Construct_UClass_USpringArmComponent_NoRegister, METADATA_PARAMS(NewProp_CameraBoom_MetaData, ARRAY_COUNT(NewProp_CameraBoom_MetaData)) };
+			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_PhysicsHandle,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_ThrowStrength,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_PullDirection,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_TK_Reach,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_BaseLookUpRate,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_BaseTurnRate,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_TKCamera,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_FollowCamera,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_CameraBoom,
+			};
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<ALianeGameCharacter>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&ALianeGameCharacter::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00800080u,
+				FuncInfo, ARRAY_COUNT(FuncInfo),
+				PropPointers, ARRAY_COUNT(PropPointers),
+				"Game",
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(ALianeGameCharacter, 493338237);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_ALianeGameCharacter(Z_Construct_UClass_ALianeGameCharacter, &ALianeGameCharacter::StaticClass, TEXT("/Script/LianeGame"), TEXT("ALianeGameCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(ALianeGameCharacter);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ MasterProjectCPP/LianeGame/Source/LianeGame/LianeGameCharacter.h
@@ -0,0 +1,128 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "LianeGameCharacter.generated.h"
+
+class UPointLightComponent;
+class USpotLightComponent;
+class UPhysicsHandleComponent;
+class USphereComponent;
+
+UCLASS(config=Game)
+class ALianeGameCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** Telekinesis camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* TKCamera;
+
+public:
+	ALianeGameCharacter();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime);
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;
+
+protected:
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/** 
+	 * Called via input to turn at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
+
+private:
+
+	// How far ahead of the player can we reach in cm
+	UPROPERTY(EditAnywhere, Category = "Telekinesis")
+	float TK_Reach;
+
+	//The direction which the grabbed object is pulled
+	UPROPERTY(EditAnywhere, Category = "Telekinesis")
+	float PullDirection;
+
+	//The strength of the telekinesis throw
+	UPROPERTY(EditAnywhere, Category = "Telekinesis")
+	float ThrowStrength;
+
+	FRotator ControlRotation;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Telekinesis", meta = (AllowPrivateAccess = "true"))
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
+
+	// Called when grab is released
+	void Release();
+
+	//Throw the object the player is grabbing
+	void Throw();
+
+	//Make player face the telekinesis object they are grabbing
+	void FaceTKObject();
+
+	//Called to move a grabbed object back/forward
+	UFUNCTION()
+	void MoveActor();
+
+	// Set (assumed) phyics handle location
+	void SetPhysicsHandleLocation();
+
+	// Return hit for first physics body in reach
+	const FHitResult GetFirstPhysicsBodyInReach();
+
+	// Returns current start of reach line
+	FVector GetTKReachLineStart();
+
+	// Returns current end of reach line
+	FVector GetTKReachLineEnd();
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	/** Returns TelekinesisCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetTKCamera() const { return TKCamera; }
+
+};
+
